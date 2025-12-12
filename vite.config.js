b2ace0b618ad94b65cd98e2855b605b72ea98f33
sofia vidotto <sofiavidotto8@gmail.com>
2025-12-12 22:45:25 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      react: path.resolve("./node_modules/react"),
-      "react-dom": path.resolve("./node_modules/react-dom"),
-    },
+  plugins: [react(), tailwindcss(),],
+  mode: 'development',  // Assicurati che l'app venga eseguita in modalità sviluppo
+  build: {
+    minify: false, // Disabilita la minificazione del codice
   },
-});
+})
